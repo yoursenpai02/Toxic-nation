6bd66f3abd45f645fc75850857e0677e3a98b418
yoursenpai02 <159107953+yoursenpai02@users.noreply.github.com>
2024-05-06 21:43:30 +0530

    Add files via upload

---
+++ Senpai/karma.py
@@ -0,0 +1,87 @@
+# <============================================== IMPORTS =========================================================>
+from pyrogram.types import InlineKeyboardButton as ib
+from telegram import InlineKeyboardButton
+
+from Mikubot import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT
+
+# <============================================== CONSTANTS =========================================================>
+START_IMG = [
+    "https://graph.org/file/0127a4b43fd20f5b03eb6.jpg",
+    "https://graph.org/file/18a995acbf326e3bb3df5.jpg",
+    "https://graph.org/file/80930fad8add5f307910c.jpg",
+    "https://graph.org/file/bac83d702ebfbffe38d1c.jpg",
+    "https://graph.org/file/7a6fbcad400bad6ded0af.jpg",
+    "https://graph.org/file/2923417b2a6feeed4fcb3.jpg",
+    "https://graph.org/file/18a995acbf326e3bb3df5.jpg",
+]
+
+HEY_IMG = "https://graph.org/file/0127a4b43fd20f5b03eb6.jpg"
+
+ALIVE_ANIMATION = [
+    "https://telegra.ph//file/f9e2b9cdd9324fc39970a.mp4",
+    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
+    "https://telegra.ph//file/c4c2759c5fc04cefd207a.mp4",
+    "https://telegra.ph//file/b1fa6609b1c4807255927.mp4",
+    "https://telegra.ph//file/f3c7147da6511fbe27c25.mp4",
+    "https://telegra.ph//file/39071b73c02e3ff5945ca.mp4",
+    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
+    "https://telegra.ph//file/6efdd8e28756bc2f6e53e.mp4",
+]
+
+FIRST_PART_TEXT = "✨ *ʜᴇʟʟᴏ* `{}` . . ."
+
+PM_START_TEXT = "✨ *ɪ ᴀᴍ ʜᴀᴛꜱᴜɴᴇ ᴍɪᴋᴜ, ᴀ  ᴠɪʀᴛᴜᴀʟ ᴘᴏᴘ ꜱᴛᴀʀ ʀᴏʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ*"
+
+START_BTN = [
+    [
+        InlineKeyboardButton(
+            text="⇦ ADD ME ⇨",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        ),
+    ],
+    [
+        InlineKeyboardButton(text="HELP", callback_data="help_back"),
+    ],
+    [
+        InlineKeyboardButton(text="DETAILS", callback_data="Miku_"),
+        InlineKeyboardButton(text="AI", callback_data="ai_handler"),
+        InlineKeyboardButton(text="SOURCE", callback_data="git_source"),
+    ],
+    [
+        InlineKeyboardButton(text="CREATOR", url=f"tg://user?id={OWNER_ID}"),
+    ],
+]
+
+GROUP_START_BTN = [
+    [
+        InlineKeyboardButton(
+            text="⇦ ADD ME ⇨",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        ),
+    ],
+    [
+        InlineKeyboardButton(text="SUPPORT", url=f"https://t.me/{SUPPORT_CHAT}"),
+        InlineKeyboardButton(text="CREATOR", url=f"tg://user?id={OWNER_ID}"),
+    ],
+]
+
+ALIVE_BTN = [
+    [
+        ib(text="UPDATES", url="https://t.me/Hydra_Updates"),
+        ib(text="SUPPORT", url="https://t.me/hydraXsupport"),
+    ],
+    [
+        ib(
+            text="⇦ ADD ME ⇨",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        ),
+    ],
+]
+
+HELP_STRINGS = """
+🫧 *Hatsune-Miku* 🫧
+
+☉ *Here, you will find a list of all the available commands.*
+
+ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : /
+"""
