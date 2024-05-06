48f94853aa8fb879164460bc1393db40debfc7ba
yoursenpai02 <159107953+yoursenpai02@users.noreply.github.com>
2024-05-06 21:36:40 +0530

    Add files via upload

---
+++ Mikubot/utils/cmdprefix.py
@@ -0,0 +1,14 @@
+from Mikubot import ALLOW_EXCL
+
+if ALLOW_EXCL:
+    CMD_STARTERS = (
+        "/",
+        "!",
+        ".",
+        "-",
+        "$",
+        "*",
+        "+",
+    )
+else:
+    CMD_STARTERS = ("/",)
